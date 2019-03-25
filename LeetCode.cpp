23957cb562e51e78bef3866a7627d55e0e317893
Su <suyuxinlong@yeah.net>
2019-03-25 11:29:54 +0800

    update

---
+++ LeetCode.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+
+/*
+	1.两数之和
+	给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+	你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+*/
+int sumOfNums(int nums[],int target) {
+	return nums[2];
+	// return 0;
+}
+
+int main(int argc, char const *argv[])
+{
+	cout << "---------- LeetCode算法刷题练习 ----------" << endl;
+	int numbers[6] = {2,3,4,5,6,7};
+	sumOfNums(numbers,1);
+	return 0;
+}
